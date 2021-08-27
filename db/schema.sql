9a872ef56d4d34e132abd9690f37f3a4121fe573
Trent Roesbery <xtrent777x@gmail.com>
2021-08-27 16:42:11 -0600

    table sql bug fix

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,25 @@
 
 
 CREATE TABLE departments (
-  id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  name VARCHAR(30) NULL,
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NULL
 );
 
 CREATE TABLE role (
-  id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NULL,
   salary DECIMAL(10,2) NULL,
   department_id INTEGER NOT NULL,
-  PRIMARY KEY (id),
   FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
-  id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NULL,
   last_name VARCHAR(30) NULL,
   role_id INTEGER NOT NULL,
   manager_id INTEGER,
   department_id INTEGER NOT NULL,
-  PRIMARY KEY (id),
   FOREIGN KEY (department_id) REFERENCES departments(id),
   FOREIGN KEY (role_id) REFERENCES role(id),
   FOREIGN KEY (manager_id) REFERENCES employees(id)
